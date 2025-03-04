671edd7fe5feefe3048e1b22adeb70218f0a433c
Enock <oswereenock@gmail.com>
2025-03-04 14:09:13 +0300

    new migration

--- base/models.py
+++ base/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from tinymce.models import HTMLField
+from ckeditor.fields import RichTextField
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100, default="Admin")
-    content = HTMLField()
+    content = RichTextField()
     cover_image = models.ImageField(upload_to='blog_covers/', default='default_cover.jpg',null=True, blank=True)
     published_at = models.DateTimeField(default=timezone.now)
 
@@ -27,7 +27,7 @@ class PracticeArea(models.Model):
     name = models.CharField(max_length=100, verbose_name="Area of Practice")
     image = models.ImageField(upload_to="practice_areas/", help_text="Upload an image representing the practice area",null=True, blank=True)
 
-    detailed_description = HTMLField(verbose_name="Detailed Description")
+    detailed_description = RichTextField(verbose_name="Detailed Description")
     slug = models.SlugField(unique=True, help_text="URL-friendly name for the practice area")
 
     def __str__(self):
--- nuru/settings.py
+++ nuru/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base',
     'django_bootstrap5',
-     'tinymce',
+    'ckeditor',
     
 ]
 
